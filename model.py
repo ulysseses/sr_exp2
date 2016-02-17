3a15f7e1bba48aa6d6fc208b453082a5199103e8
ulysseses <vlchen91@gmail.com>
2016-02-17 10:43:09 +0900

    Update model.py

--- model.py
+++ model.py
@@ -68,139 +68,6 @@ def _arr_initializer(arr):
     return _initializer
 
 
-def _st(x, thresh, name='st'):
-    """
-    L1 Soft threshold operator.
-
-    Args:
-      x: input
-      thresh: threshold variable
-      name: name assigned to this operation
-    Returns:
-      soft threshold of `x`
-    """
-    with tf.name_scope('st'):
-        return tf.mul(tf.sign(x), tf.nn.relu(tf.nn.bias_add(tf.abs(x), -thresh)))
-
-
-def _lcod(x, w_e, w_s, thresh, conf):
-    """
-    Learned Coordinate Descent (LCoD). LCoD is an approximately sparse encoder. It
-    approximates (in an L2 sense) a sparse code of `x` according to dictionary `w_e`.
-    Note that during backpropagation, `w_e` isn't strictly a dictionary (i.e.
-    dictionary atoms are not strictly normalized).
-
-    LCoD is a differentiable version of greedy coordinate descent.
-
-    Args:
-      x: [n, n_f] tensor
-      w_e: [n_f, n_c] encoder tensor
-      w_s: [n_c, n_f] mutual inhibition tensor
-      thresh: soft thresold
-      conf: configuration dictionary
-    Returns:
-      z: LCoD output
-    """
-    def f1():
-        '''k_i == 0'''
-        forget_z_i = tf.concat(1, [z_ik, tf.zeros([1, n_c - 1],
-                                                  dtype=tf.float32)],
-                               name='forget_z_%d' % i)
-        update_z_i = tf.concat(1, [z_bar_ik, tf.zeros([1, n_c - 1],
-                                                      dtype=tf.float32)],
-                               name='update_z_%d' % i)
-        return forget_z_i, update_z_i
-
-    def f2():
-        '''k_i == n_c - 1'''
-        forget_z_i = tf.concat(1, [tf.zeros([1, n_c - 1], dtype=tf.float32),
-                                   z_ik],
-                               name='forget_z_%d' % i)
-        update_z_i = tf.concat(1, [tf.zeros([1, n_c - 1], dtype=tf.float32),
-                                   z_bar_ik],
-                               name='update_z_%d' % i)
-        return forget_z_i, update_z_i
-
-    def f3():
-        '''k_i > 0 and k_i < n_c - 1'''
-        forget_z_i = tf.concat(1, [tf.zeros(tf.pack([1, k_i]), dtype=tf.float32),
-                                   z_ik,
-                                   tf.zeros(tf.pack([1, n_c - (k_i + 1)]),
-                                            dtype=tf.float32)],
-                               name='forget_z_%d' % i)
-        update_z_i = tf.concat(1, [tf.zeros(tf.pack([1, k_i]), dtype=tf.float32),
-                                   z_bar_ik,
-                                   tf.zeros(tf.pack([1, n_c - (k_i + 1)]),
-                                            dtype=tf.float32)],
-                               name='update_z_%d' % i)
-        return forget_z_i, update_z_i
-
-    n = conf['mb_size'] * (conf['iw'] // conf['ps'])**2
-    n_c = conf['n_c']
-    T = conf['T']
-    n_f = conf['n_chans'] * conf['pw']**2
-
-    prevs1 = [None for _ in range(n)]
-    prevs2 = [None for _ in range(n)]
-    with tf.name_scope('slice_x'):
-        for i in range(n):
-            x_i = tf.slice(x, [i, 0], [1, n_f], name='x_%d' % i)
-            prevs1[i] = x_i
-
-    with tf.name_scope('itr_00'):
-        for i in range(n):
-            x_i = prevs1[i]
-            b_i = tf.matmul(x_i, w_e, name='b_%d' % i)
-            z_i = tf.zeros_like(b_i, dtype=tf.float32, name='z_%d' % i)
-            prevs1[i] = b_i
-            prevs2[i] = z_i
-
-    for t in range(1, T):
-        with tf.name_scope('itr_%02d' % t):
-            for i in range(n):
-                b_i = prevs1[i]
-                z_i = prevs2[i]
-                z_bar_i = _st(b_i, thresh, name='z_bar_%d' % i)
-                with tf.name_scope('greedy_heuristic'):
-                    if t > 1:
-                        tmp_i = z_bar_i - z_i
-                    else:
-                        tmp_i = z_bar_i
-                    tmp2_i = tf.abs(tmp_i)
-                    k_i = tf.to_int32(tf.argmax(tmp2_i, 1, name='k_%d' % i))
-                    e_i = tf.reshape(tf.slice(tmp_i, tf.pack([0, k_i]), tf.pack([1, 1]),
-                                              name='e_%d' % i),
-                                     [])
-                with tf.name_scope('update_b'):
-                    s_slice_i = tf.slice(w_s, tf.pack([k_i, 0]), tf.pack([1, n_c]),
-                                         name='s_slice_%d' % i)
-                    b_i = tf.add(b_i, e_i * s_slice_i, name='b_%d' % i)
-                with tf.name_scope('update_z'):
-                    z_bar_ik = tf.slice(z_bar_i, tf.pack([0, k_i]), tf.pack([1, 1]),
-                                        name='z_bar_%dk' % i)
-                    z_ik = tf.slice(z_i, tf.pack([0, k_i]), tf.pack([1, 1]),
-                                    name='z_%dk' % i)
-                    tup_i = control_flow_ops.case({tf.equal(k_i, 0): f1,
-                                                   tf.equal(k_i, n_c - 1): f2},
-                                                  default=f3,
-                                                  exclusive=False)
-                    forget_z_i, update_z_i = tup_i
-                    z_i = tf.add_n([z_i, -forget_z_i, update_z_i], name='z_%d' % i)
-                prevs1[i] = b_i
-                prevs2[i] = z_i
-
-    with tf.name_scope('itr_%02d' % T):
-        for i in range(n):
-            b_i = prevs1[i]
-            z_i = _st(b_i, thresh, name='z_%d' % i)
-            prevs2[i] = z_i
-
-    # Concatenate to full tensor
-    z = tf.concat(0, prevs2, name='z')
-
-    return z
-
-
 def _low_rank_approx(a, low_rank):
     """
     Use SVD to approximate `a` as two matrices, `b` and `c`.
@@ -249,6 +116,99 @@ def _low_rank_helper(mat, low_rank, suffix):
     return w_mat
 
 
+def _st(x, thresh, name='st'):
+    """
+    L1 Soft threshold operator.
+
+    Args:
+      x: input
+      thresh: threshold variable
+      name: name assigned to this operation
+    Returns:
+      soft threshold of `x`
+    """
+    with tf.name_scope('st'):
+        return tf.mul(tf.sign(x), tf.nn.relu(tf.nn.bias_add(tf.abs(x), -thresh)))
+
+
+def _lista(x, w_e, w_s, thresh, T):
+    """
+    Learned Iterative Shrinkage-Thresholding Algorithm (LISTA). LISTA is an
+    approximately sparse encoder. It approximates (in an L2 sense) a sparse code
+    of `x` according to dictionary `w_e`. Note that during backpropagation, `w_e`
+    isn't strictly a dictionary (i.e. dictionary atoms are not strictly normalized).
+    LISTA is a differentiable version of the iterative FISTA algorithm.
+    Args:
+      x: [n, n_f] tensor
+      w_e: [n_f, n_c] encoder tensor
+      w_s: [n_c, n_f] mutual inhibition tensor
+      thresh: soft threshold
+      T: number of iterations
+    Returns:
+      z: LISTA output
+    """
+    b = tf.matmul(x, w_e, name='b')
+    with tf.name_scope('itr_00'):
+        z = _st(b, thresh, name='z')
+    for t in range(1, T+1):
+        with tf.name_scope('itr_%02d' % t):
+            c = b + tf.matmul(z, w_s, name='c')
+            z = _st(c, thresh, name='z')
+    return z
+
+
+def _lcod(x, w_e, w_s, thresh, T):
+    """
+    Learned Coordinate Descent (LCoD). LCoD is an approximately sparse encoder. It
+    approximates (in an L2 sense) a sparse code of `x` according to dictionary `w_e`.
+    Note that during backpropagation, `w_e` isn't strictly a dictionary (i.e.
+    dictionary atoms are not strictly normalized).
+
+    LCoD is a differentiable version of greedy coordinate descent.
+
+    Args:
+      x: [n, n_f] tensor
+      w_e: [n_f, n_c] encoder tensor
+      w_s: [n_c, n_f] mutual inhibition tensor
+      thresh: soft thresold
+      T: number of iterations
+    Returns:
+      z: LCoD output
+    """
+    with tf.name_scope('itr_00'):
+        b = tf.matmul(x, w_e, name='b')
+        z = tf.zeros_like(b, dtype=tf.float32, name='z')
+
+    for t in range(1, T):
+        with tf.name_scope('itr_%02d' % t):
+            z_bar = _st(b, thresh, name='z_bar')
+            with tf.name_scope('greedy_heuristic'):
+                # no tf.tile b/c tf.select will brodcast?
+                if t > 1:
+                    z_diff = tf.sub(z_bar, z, name='z_diff')
+                else:
+                    z_diff = z_bar
+                abs_z_diff = tf.abs(z_diff, name='abs_z_diff')
+
+                tmp = tf.reduce_max(abs_z_diff, 1, True)
+                tmp2 = tf.equal(abs_z_diff, tmp)
+                e = select(tmp2, z_diff, tf.zeros_like(z_bar, dtype=tf.float32),
+                           name='e')
+                ks = tf.argmax(abs_z_diff, 1, name='ks')
+                
+            with tf.name_scope('update_b'):
+                s_slices = tf.gather(w_s, ks, name='s_slices')
+                b = tf.add(b, tf.mul(e, s_slices), name='b')
+
+            with tf.name_scope('update_z'):
+                z = tf.select(tmp2, z_bar, z, name='z')
+
+    with tf.name_scope('itr_%02d' % T):
+        z = _st(b, thresh, name='z')
+
+    return z
+
+
 def inference(x, conf):
     """
     Sparse Coding Based Network for Super-Resolution. This is a convolutional
@@ -259,33 +219,37 @@ def inference(x, conf):
     `T` iterations. The LISTA output patches are de-normalized with `scale_norm`
     and stitched back into their respective positions to re-construct the final
     output image.
-
     Args:
       x: input layer
       conf: configuration dictionary
     Returns:
       y: output
     """
-    mb_size = conf['mb_size']
-    iw = conf['iw']
     fw = conf['fw']
     pw = conf['pw']
     ps = conf['ps']
     n_chans = conf['n_chans']
     n_c = conf['n_c']
+    T = conf['T']
     cw = conf['cw']
     thresh0 = conf['thresh0']
+    subnet_name = conf['subnet_name'].lower()
     e_rank = conf['e_rank']
     s_rank = conf['s_rank']
     d_rank = conf['d_rank']
     
     n_f = n_chans*pw*pw
-    bs = mb_size
+    
+    if subnet_name == 'lista':
+        subnet = _lista
+    elif subnet_name == 'lcod':
+        subnet = _lcod
+    else:
+        raise ValueError('subnet_name must be "lista" or "lcod"')
     
     # Initialize constant filters
     with tf.variable_scope('const_filt'):
-        w_shift1 = tf.constant(_init_shift(n_chans, pw), name='w_shift1')
-        w_shift2 = tf.constant(_init_shift(1, pw), name='w_shift2')
+        w_shift = tf.constant(_init_shift(n_chans, pw), name='w_shift')
         w_stitch = tf.constant(_init_stitch(pw), name='w_stitch')
     
     # Initialize feature extraction filters
@@ -293,23 +257,30 @@ def inference(x, conf):
         w_conv = tf.get_variable('w_conv', [fw, fw, 1, n_chans], tf.float32,
             initializer=tf.truncated_normal_initializer(0., _relu_std(fw, 1)))
     
+    # Get dimensions
+    with tf.name_scope('dimensions'):
+        bs, h, w = [tf.shape(x)[i] for i in range(3)]
+        bs = tf.identity(bs, name='bs')
+        h = tf.identity(h, name='h')
+        w = tf.identity(w, name='w')
+    
     # Feature Extraction
-    # [bs, iw, iw, 1] -> [bs, iw, iw, n_chans]
+    # [bs, h, w, 1] -> [bs, h, w, n_chans]
     x_conv = tf.nn.conv2d(x, w_conv, [1, 1, 1, 1], 'SAME', name='x_conv')
     
     # Shift with pw*pw dirac delta filters to create overlapping patches.
     # Only obtain patches every `ps` strides.
     # A patch is resembled as the flattened array along the last dimension.
-    # [bs, iw, iw, n_chans] --> [bs, iw//ps, iw//ps, n_chans*pw*pw]
-    x_shift = tf.nn.depthwise_conv2d(x_conv, w_shift1, [1, ps, ps, 1], 'SAME',
+    # [bs, h, w, n_chans] --> [bs, h//ps, w//ps, n_chans*pw*pw]
+    x_shift = tf.nn.depthwise_conv2d(x_conv, w_shift, [1, ps, ps, 1], 'SAME',
         name='x_shift')
     
     # 4D tensor -> matrix
-    # [bs, iw//ps, iw//ps, n_chans*pw*pw] -> [bs*(iw//ps)*(iw//ps), n_chans*pw*pw]
-    x_in = tf.reshape(x_shift, [bs*(iw//ps)*(iw//ps), n_f], name='x_in')
+    # [bs, h//ps, w//ps, n_chans*pw*pw] -> [bs*(h//ps)*(w//ps), n_chans*pw*pw]
+    x_in = tf.reshape(x_shift, [-1, n_f], name='x_in')
     
     # Feed into sub-network
-    with tf.variable_scope('lcod'):
+    with tf.variable_scope(subnet_name):
         with tf.device('/cpu:0' if FLAGS.dev_assign else None):
             thresh = tf.get_variable('thresh',
                 [n_c],
@@ -318,7 +289,12 @@ def inference(x, conf):
             
             # Initial values
             e = np.random.randn(n_f, n_c).astype(np.float32) * _relu_std(1, n_f)
-            s = (np.eye(n_c) - e.T.dot(e)).astype(np.float32)
+            if subnet_name == 'lista':
+                L = 5.
+                e /= L
+                s = (np.eye(n_c) - e.T.dot(e) / L).astype(np.float32)
+            else:
+                s = (np.eye(n_c) - e.T.dot(e)).astype(np.float32)
             d = np.random.randn(n_c, pw*pw).astype(np.float32) * _relu_std(1, n_c)
             # Encoder
             w_e = _low_rank_helper(e, e_rank, 'e')
@@ -328,25 +304,25 @@ def inference(x, conf):
             w_d = _low_rank_helper(d, d_rank, 'd')
         
         # Sub-network
-        # [bs*(iw//ps)*(iw//ps), n_f] -> [bs*(iw//ps)*(iw//ps), n_c]
-        z = _lcod(x_in, w_e, w_s, thresh, conf)
+        # [bs*(h//ps)*(w//ps), n_f] -> [bs*(h//ps)*(w//ps), n_c]
+        z = subnet(x_in, w_e, w_s, thresh, T)
         
         # Decode
-        # [bs*(iw//ps)*(iw//ps), n_c] -> [bs*(iw//ps)*(iw//ps), pw*pw]
+        # [bs*(h//ps)*(w//ps), n_c] -> [bs*(h//ps)*(w//ps), pw*pw]
         y_out = tf.matmul(z, w_d, name='y_out')
         
     # matrix --> 4D tensor
-    # [bs*(iw//ps)*(iw//ps), pw*pw] -> [bs, iw//ps, iw//ps, pw*pw]
-    y_shift = tf.reshape(y_out, [bs, iw // ps, iw // ps, pw*pw],
+    # [bs*(h//ps)*(w//ps), pw*pw] -> [bs, h//ps, w//ps, pw*pw]
+    y_shift = tf.reshape(y_out, tf.pack([bs, h // ps, w // ps, pw*pw]),
                          name='y_shift')
     
     # Average overlapping images together
     with tf.name_scope('overlap_avg'):
-        mask_input = tf.ones([bs, iw // ps, iw // ps, pw*pw],
+        mask_input = tf.ones(tf.pack([bs, h // ps, w // ps, pw*pw]),
                              dtype=tf.float32)
         mask = tf.nn.deconv2d(mask_input, w_stitch,
-            [bs, iw, iw, 1], [1, ps, ps, 1], 'SAME', name='mask')
-        y_overlap = tf.nn.deconv2d(y_shift, w_stitch, [bs, iw, iw, 1],
+            tf.pack([bs, h, w, 1]), [1, ps, ps, 1], 'SAME', name='mask')
+        y_overlap = tf.nn.deconv2d(y_shift, w_stitch, tf.pack([bs, h, w, 1]),
             [1, ps, ps, 1], 'SAME', name='y_overlap')
         y_average = tf.div(y_overlap, mask + 1e-8, name='y_average')
     
@@ -356,7 +332,7 @@ def inference(x, conf):
     # Crop to remove convolution boundary effects
     with tf.name_scope('crop'):
         crop_begin = [0, cw, cw, 0]
-        crop_size = [bs, iw - 2*cw, iw - 2*cw, 1]
+        crop_size = tf.pack([-1, h - 2*cw, w - 2*cw, 1])
         pred_crop = tf.slice(pred0, crop_begin, crop_size, name='pred_crop')
     
     pred = tf.identity(pred_crop, name='pred')
